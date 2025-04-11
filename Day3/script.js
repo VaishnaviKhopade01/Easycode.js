3fada4d7cff3167630e42ede07112ba7b9c624c2
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-04-11 15:18:25 +0530

    datatypes page added

--- Day3/script.js
+++ Day3/script.js
@@ -1,25 +1,58 @@
 let radius = 7
 
-console.log(radius);
+// console.log(radius);
 
-var name = vaishnavi
+var name = "vaishnavi"
 
-console.log(name)
+// console.log(name)
 
 let tag_line12 = "we start learning JS"
 
-console.log(tag_line12)
+// console.log(tag_line12)
 
-boolean = true || false
-
-let a
- 
-console.log(typeof a)
+// boolean = true || false
 
 let PI=3.14
-let name="vaishnavi"
-let age=20
-let radius=7
+// console.log(PI)
 
-console.info(name)
+let age=20
+// console.log(typeof(age))
+
+let pincode=412206
+// console.log(pincode)
  
+// string
+
+let emailId="khopadevaishnavi2004@gmail.com"
+// console.log(emailId)
+
+var college_name="ATC Bhor"
+// console.log(college_name)
+
+let city_name="Pune"
+console.log(city_name)
+
+//boolean
+
+let userlogin=true
+let user_have_access=false
+let ban=false
+
+//Null
+
+let a=null
+
+//undefined
+
+let x
+console.log(typeof(x))
+
+//non-primitive
+
+let obj= {
+    name:"vaishnav",
+    city:"pune"
+}
+console.log(obj)
+
+let gret=["arya","pune","hello"]
---
+++ Day4/script.js
@@ -0,0 +1,11 @@
+// console.log(document)
+
+let xyz="vaishnavi"
+
+// console.log(xyz)
+
+const PI=3.14
+// console.log(PI)
+
+let name="riya"
+console.log(name)
