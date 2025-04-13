c0725af216961f8dff494c6283d81283b60d528a
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-04-13 14:53:45 +0530

    learn about operators in js

---
+++ Day5/script.js
@@ -0,0 +1,73 @@
+let a=10
+// console.log(a)
+
+let x=3
+x+=5            //x=x+5
+// console.log(x)
+
+let abc=5
+abc+=2
+// console.log(abc)
+
+let b=10
+b-=4
+// console.log(b)
+
+let p=6
+p*=3
+// console.log(p)
+
+let n=9
+n/=3
+// console.log(n)
+
+let r=24
+r%=2
+// console.log(r)
+
+let d=18
+d%=5
+// console.log(d)
+
+//comparison operator
+
+let m=2
+let l=3
+// console.log(m==l)
+
+let s=5
+let y=5
+// console.log(s==y)
+
+let q=12
+let u=12
+// console.log(q!=u)
+
+let ab=3
+let mn=4
+// console.log(ab!=mn)
+
+let z=7
+let w="7"
+// console.log(z===w)
+// console.log(z==w)
+
+let OTP=1234
+let User_OTP="1234"
+// console.log(OTP==User_OTP)
+// console.log(OTP===User_OTP)
+
+let lmn=20
+let pqr=10
+// console.log(20>10)
+// console.log(20<10)
+
+// let result=20>10
+// console.log(result)
+
+let wx=6
+let qr=4
+
+let result=6>=4
+// console.log(result)
+
