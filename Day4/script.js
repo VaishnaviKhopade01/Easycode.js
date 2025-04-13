34912de81cd166e69a5e4cd1ac9051ccf679f674
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-04-13 13:47:39 +0530

    learn about console & operator

--- Day4/script.js
+++ Day4/script.js
@@ -1,11 +1,63 @@
-// console.log(document)
 
-let xyz="vaishnavi"
+let tag_line="let's learn javascript"
+// console.info(tag_line)
 
-// console.log(xyz)
+console.warn("please login fast to access this page")
 
-const PI=3.14
-// console.log(PI)
+console.warn("please connect to network")
 
-let name="riya"
-console.log(name)
+console.error("user ban")
+
+let err="slow network detected"
+// console.error(err)
+
+let message="hello world"
+// console.log(message)
+
+let err_msg="signal is not available"
+// console.error(err_msg)
+
+let warn_msg="failed to fetch"
+console.warn(warn_msg)
+
+console.clear()
+
+//Operators in js
+
+let a=10
+let b=11
+
+//addition
+console.log(a+b)
+
+//subtraction
+console.log(a-b)
+
+//multiplication
+console.log(a*b)
+
+//division
+console.log(a|b)
+
+//modulus
+console.log(a%b)
+
+let x=5
+let y=4
+
+console.log(x+y)
+
+console.log(x-y)
+
+console.log(x*y)
+
+console.log(x|y)
+
+console.log(x%y)
+
+//eg3
+
+let first_number=6
+let second_number=2
+
+console.log(first_number+second_number)
