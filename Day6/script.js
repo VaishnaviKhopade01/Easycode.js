4a04ff24f86e678ccd1fec569bebcb2c1f4bced2
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-04-16 19:44:15 +0530

    learning ternary operators

---
+++ Day6/script.js
@@ -0,0 +1,31 @@
+// let age=21
+let age=8
+
+// let result=age>=18?"able to vote":"not able to vote"
+// console.log(result)
+
+let gender="m"
+// let result = gender=="m"?"male":"female"
+// console.log(result)
+
+let number=8
+// let result=number>0?"number is positive":"number is negative"
+// console.log(result)
+
+// let num=4
+// let res=num%2==0?"num is even":"num is odd"
+// console.log(res)
+
+let num=9
+// let res=num%2==0?"num is even":"num is odd"
+// console.log(res)
+
+let num1=124
+let result=num1%3==0?"num1 is divisible by 3":"num1 is not divisible by 3"
+// console.log(result)
+
+let user="abcdef"
+let res=user=="abcdef"?"valid password":"valid password"
+console.log(res)
+
+
