ff73a5795db8609fa4e247811efa9a49e28f0035
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-17 10:47:43 +0530

    learning about alert box in javascript

---
+++ Day8/script.js
@@ -0,0 +1,33 @@
+// alert("you have lost your network")
+
+// let result=("confirm you are sure")
+// console.log(result)
+
+// let result = prompt("Enter your name");
+
+// document.writeln(" <h1> hello"  + result +  " welcome to javascript </h1>");
+
+
+// let name = prompt("enter your age")
+
+// console.log(name)
+// console.log(typeof(name))
+
+
+
+// let num1 = prompt("Enter first value:")
+// let num2 = prompt("Enter second value:")
+
+// let ope1 = parseInt (num1)
+// let ope2 = parseInt (num2)
+
+// console.log("Addition:" + (ope1 + ope2))
+
+// let a = "Hello"
+// let b = "Everyone"
+
+// console.log(a + b)
+
+let age = parseInt("Enter your age")
+console.log(typeof(age))
+
