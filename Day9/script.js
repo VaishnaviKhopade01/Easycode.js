7526aaec7420f3b08a53270d5a5fee65de437d95
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-18 13:46:33 +0530

    learning about conditional statements if, elseif

---
+++ Day9/script.js
@@ -0,0 +1,20 @@
+// let num = -12
+
+// if(num>0){
+//     console.log("Number is positive")
+// }else{
+//     console.log("Number is negative")
+// }
+
+
+// let num = prompt("Enter a value")
+// if(num%2 == 0){
+//     console.log("The value is even")
+// }else{
+//     console.log("The value is odd")
+// }
+
+
+// if(true){
+//     console.log("hello")
+// }
