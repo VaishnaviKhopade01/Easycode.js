d1b06356b7680955a284a377581675635ad6f8bc
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-18 16:01:05 +0530

    learning about array methods

---
+++ Day11(Array)/script.js
@@ -0,0 +1,98 @@
+// let colors =['yellow','blue','green','pink','grey']
+
+// document.writeln(colors[0])
+// document.writeln(colors[2])
+// document.writeln(colors[4])
+
+// console.log(colors[0])
+// console.log(colors[1])
+// console.log(colors[2])
+// console.log(colors[3])
+// console.log(colors[4])
+
+
+// let todos = ['wake up']
+
+// todos.push('start running')
+// todos.push('ready to college')
+// todos.push('start learning different languages')
+
+// console.log(todos)
+
+// let cars = ["thar"]
+
+// cars.push("bugati")
+// cars.push("scarpio")
+// cars.push("swift")
+
+// console.log(cars)
+
+// let numbers = [100]
+
+// numbers.push(200)
+// numbers.push(300)
+// numbers.push(400)
+// numbers.push(500)
+
+// console.log(numbers)
+
+// numbers.unshift(50)
+// numbers.unshift(30)
+// numbers.unshift(10)
+
+// console.log(numbers)
+
+
+// let places = ["pune"]
+
+// places.push("mumbai")
+// places.push("delhi")
+// places.push("london")
+// places.unshift("satara")
+
+// console.log(places)
+
+// let Myname = ["s","a","t","a","r","a","w"]
+
+// console.log(Myname)
+
+// let removedlm = Myname.pop()
+
+// console.log(removedlm)
+// console.log(Myname)
+
+// let Grade = ["A","B","C","D","E","F"]
+// console.log(Grade)
+
+// Grade.pop()
+// Grade.push("Fail")
+// Grade.unshift("A+")
+
+// console.log(Grade)
+
+// Grade.shift()
+// console.log(Grade)
+
+
+
+// let request = ["Vaishanvi"]
+
+// if(request.length<=0){
+//     console.log("no one is here")
+// }else{
+//     console.log("some request")
+// }
+
+
+let mobilecomapany = ["Apple"]
+
+mobilecomapany.push("Samsung")
+mobilecomapany.unshift("Realme")
+mobilecomapany.push("Redme")
+console.log(mobilecomapany)
+
+mobilecomapany.pop()
+
+console.log(mobilecomapany)
+
+console.log(mobilecomapany[2])
