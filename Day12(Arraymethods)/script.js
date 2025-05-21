65463f9bb93159061067efda9488d3ae449722b2
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-21 11:50:50 +0530

    learning about array methods

---
+++ Day12(Arraymethods)/script.js
@@ -0,0 +1,51 @@
+// let num = [1,2,3,4,5,6]
+// let abc = ['a','b','c','d','e','f']
+
+// num.push(7)
+// num.unshift(0)
+// num.pop()
+// num.shift()
+
+// abc.reverse()
+
+// console.log(num)
+// console.log(abc)
+
+
+// console.log(abc.includes('m'))
+// console.log(abc.includes('b'))
+// console.log(num.includes(4))
+
+
+// let cities = ["Mumbai","Delhi","Satara","Pune","Nashik"]
+
+// cities.push("Nanded")
+// console.log(cities)
+
+// console.log(cities.reverse())
+
+// console.log(cities.sort())
+
+// console.log(cities.includes("Pune"))
+
+// console.log(cities.indexOf("Bhor"))
+
+// console.log(cities.toString())
+
+// console.log(typeof(cities.toString()))
+
+// console.log(cities.slice())
+
+// console.log(cities.slice(0,5))
+
+// console.log(cities.slice(3,6))
+
+
+let friends = ["Shivani","kiran","ashwini","shreya","dhanshree","Priya","khushi"]
+
+friends.push("ankita")
+friends.unshift("seema")
+
+console.log(friends)
+
+console.log(friends.indexOf("kiran"))
