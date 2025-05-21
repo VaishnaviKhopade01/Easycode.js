be2c946574b2ff9763a273a5c944b46e7e90ca38
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-21 12:20:47 +0530

    object and its methods

---
+++ Day13(object)/script.js
@@ -0,0 +1,45 @@
+// let Myinfo = {
+//     name : "vaishnavi",
+//     age : 21,
+//     city : "Pune",
+//     std : "sybcs"
+// }
+
+// console.log(Myinfo)
+
+// let myinfovalues = Object.values(Myinfo)
+
+// console.log(myinfovalues)
+
+// let myinfokeys = Object.keys(Myinfo)
+// console.log(myinfokeys)
+
+
+
+// let userData = {
+//     name : "Riya",
+//     age : 25,
+//     profile_picture : "profile.jpg"
+// }
+
+// console.log(userData)
+// userData.name = "vaishanvi"
+
+// console.log(userData)
+
+let phoneInfo = {
+    name : "Samsung",
+    price : 18000,
+    color : "Grey",
+    battery : 5000
+}
+
+console.log(phoneInfo)
+
+delete phoneInfo.battery
+
+let phoneinfovalues = Object.values(phoneInfo)
+console.log(phoneinfovalues)
+
+phoneInfo.Ram = 8 
+console.log(phoneInfo)
