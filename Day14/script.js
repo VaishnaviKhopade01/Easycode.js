c0b3de89961508695bea6b89fd8162d2e7c496b3
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-22 15:00:40 +0530

    learning about while and do-while loop

--- Day14/script.js
+++ Day14/script.js
@@ -1,3 +1,58 @@
-while(true){
-    console.log("hello")
-}
+// while(true){
+//     console.log("hello")
+// }
+
+// let i = 0
+// while(i<=10){
+//     console.log(i)
+//     document.writeln(i)
+//     i++
+// }
+
+// let count = 0
+// while(count<11){
+//     console.log(count)
+//     count = count + 1
+// }
+
+
+// let num = 0
+// while(num<101){
+//     if(num%2==0){
+//         console.log(num)
+//         num++
+//     }
+// }
+
+
+// let num = 0
+// while(num<=100){
+//     document.writeln(num + "<br>")
+//     num++
+    
+// }
+
+
+// let num = Number(prompt("Enter the number"))
+
+// let count = 1;
+// while(count<=10){
+//     document.writeln("<h1>" + num*count +"</h1>")
+//     count++
+// }
+
+
+// let no = 0
+// do{
+//     console.log(no)
+//     no++
+// }while(no<5)
+
+
+let num = 10
+do{
+    console.log("Hello Everyone")
+    num++
+    console.log(num)
+
+}while(num<=15)
