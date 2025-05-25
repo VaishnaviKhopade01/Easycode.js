3106f1e91a4ec768f1ca3963559837f26d46a079
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-25 15:31:00 +0530

    learn about strings

---
+++ Day13(String)/script.js
@@ -0,0 +1,23 @@
+// let name = "Easycode"
+
+// console.log(name.toLowerCase())
+// console.log(name.toUpperCase())
+
+
+// let msg = "welcome to easycode website"
+// console.log(msg.split(""))
+
+// console.log(msg.split(" "))
+
+
+// let Username = "vaishnavi khopade"
+
+// console.log(Username.split(" ")[0])
+
+
+let Tmsg = "hello how are you"
+
+console.log(Tmsg.split("T"))
+
+let str = "vaishnavi"
+console.log(str.length())
---
+++ Day15/script.js
@@ -0,0 +1,10 @@
+// let a = "hello"
+// console.log(a)
+
+
+// let a = 5
+// while(a<=15){
+//     console.log("hello world")
+//     a++
+// }
+
